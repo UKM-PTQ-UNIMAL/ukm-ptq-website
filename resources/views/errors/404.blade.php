07e57727de056ed939cc9973ed79a6952c8ec857
Khairul Aqram <khairulaqram21@gmail.com>
2024-06-07 00:09:02 +0700

    update user view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,21 +3,24 @@
 @endphp
 
 @extends('layouts.main')
-  @section('container')
+@section('container')
     <section class="notfound">
-      <div class="container my-lg-5 py-lg-5 ">
-        <div class="row justify-content-center px-5">
-          <div class="col-md-5  align-self-center">
-            <img src="{{ url('img/404.png') }}"  class="img-fluid" alt="sad">
-          </div>
-          <div class="col-md-6  align-self-center text-center text-md-start">
-            <h1>404</h1>
-            <h2>UPPSSS !!.</h2>
-            <p class="text-muted">Ga ada apa apa disini ngab, halaman tidak tersedia. Silahkan hubungi kami apabila ini merupakan kesalahan
-            </p>
-            <a href="/" class="text-warning"><p><i class="bi bi-arrow-left"></i> Back To Home</p></a>
-          </div>
+        <div class="container my-lg-5 py-lg-5 ">
+            <div class="row justify-content-center px-5">
+                <div class="col-md-5  align-self-center">
+                    <img src="{{ url('img/404.png') }}" class="img-fluid" alt="sad">
+                </div>
+                <div class="col-md-6  align-self-center text-center text-md-start">
+                    <h1>404</h1>
+                    <h2>UPPSSS !!.</h2>
+                    <p class="text-muted">Maaf kak, halaman ini tidak tersedia. Silahkan hubungi kami apabila
+                        ini merupakan kesalahan
+                    </p>
+                    <a href="/" class="text-primary">
+                        <p><i class="bi bi-arrow-left"></i> Back To Home</p>
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
     </section>
-  @endsection
+@endsection
